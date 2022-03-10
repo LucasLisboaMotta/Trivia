99216eb10dbd7dc924c08483f04a3f600f5c805e
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-10 15:59:00 -0300

    Colocando frase de feedback no header (requisito 13)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,18 @@ import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
-    const { name, score, gravatarEmail } = this.props;
+    const { name, score, gravatarEmail, assertions } = this.props;
+    const MINIMUM_ASSERTIONS = 3;
     return (
       <header>
         <span data-testid="header-player-name">{name}</span>
         <span data-testid="header-score">{score}</span>
         <img src={ gravatarEmail } alt={ name } data-testid="header-profile-picture" />
+        <span data-testid="feedback-text">
+          { assertions < MINIMUM_ASSERTIONS
+            ? 'Could be better...'
+            : 'Could be better...'}
+        </span>
       </header>
     );
   }
@@ -19,12 +25,14 @@ const mapStateToProps = ({ player }) => ({
   name: player.name,
   score: player.score,
   gravatarEmail: player.gravatarEmail,
+  assertions: player.assertions,
 });
 
 Header.propTypes = {
   name: PropTypes.string.isRequired,
   gravatarEmail: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
