f72773f7f542446737fca0d8de0b9d6a242d0474
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-10 16:36:41 -0300

    construindo tela de feedback (requsito 14)

---
+++ src/pages/FeedBack.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class FeedBack extends Component {
+  render() {
+    const { assertions, score } = this.props;
+    const MINIMUM_ASSERTIONS = 3;
+    return (
+      <div>
+        <h1 data-testid="feedback-text">
+          { assertions < MINIMUM_ASSERTIONS
+            ? 'Could be better...'
+            : 'Could be better...'}
+        </h1>
+        <p>
+          You got
+          {' '}
+          <span data-testid="feedback-total-question">{ assertions }</span>
+          {' '}
+          questions right
+        </p>
+        <p>
+          Making
+          {' '}
+          <span data-testid="feedback-total-score">{score}</span>
+          {' '}
+          points
+        </p>
+        <p />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ player }) => ({
+  assertions: player.assertions,
+  score: player.score,
+});
+
+FeedBack.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(FeedBack);
